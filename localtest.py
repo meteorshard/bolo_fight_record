fd037b837061d9b509dcd9beeb47bd53023709df
meteorshard <doooomsday@gmail.com>
2017-01-10 16:18:58 +0800

    web服务部署好了

--- localtest.py
+++ localtest.py
@@ -2,36 +2,62 @@
 
 from classes.fighter import Fighter
 from pymongo import MongoClient
+from flask import Flask, request
+import re
 
-def tt_localtest():
-    test_fighter = Fighter('Ronda Rou')
-    print(test_fighter.name)
-    print(test_fighter.aka)
-    print(test_fighter.weightclass)
-    # print(repr(test_fighter.fightrecord))
+app = Flask(__name__)
 
-    tt_client = MongoClient()
-    db = tt_client.bolofightrecord
+@app.route('/')
+def index():
+    return process(request.args.get('fighter').replace('%20',' '))
+
+def process(name):
+
+    print('dafsdfasdfasfasdfasdf')
+
+    bolofightrecord_client = MongoClient()
+    db = bolofightrecord_client.bolofightrecord
     fighters = db.fighters
 
-    cursor = fighters.find({'name':test_fighter.name})
-
+    cursor = fighters.find({'name':re.compile(name,re.IGNORECASE)})
     if cursor.count() > 0:
         print('Found!')
         for fighterfound in cursor:
             print(repr(fighterfound))
+            return fighterfound['name']
     else:
-        print('Not found! Insert new record...')
-        result = fighters.insert_one(
-            {
-                'name': test_fighter.name,
-                'aka': test_fighter.aka,
-                'fightrecord':test_fighter.fightrecord
-            }
-        )
-        if result:
-            print('Done.')
+        return 'Not found'
+
+
+# def tt_localtest():
+#     test_fighter = Fighter('sdfaksdlfasd')
+#     print(test_fighter.name)
+#     print(test_fighter.aka)
+#     print(test_fighter.weightclass)
+#     # print(repr(test_fighter.fightrecord))
+
+#     tt_client = MongoClient()
+#     db = tt_client.bolofightrecord
+#     fighters = db.fighters
+
+#     cursor = fighters.find({'name':test_fighter.name})
+
+#     if cursor.count() > 0:
+#         print('Found!')
+#         for fighterfound in cursor:
+#             print(repr(fighterfound))
+#     else:
+#         print('Not found! Insert new record...')
+#         result = fighters.insert_one(
+#             {
+#                 'name': test_fighter.name,
+#                 'aka': test_fighter.aka,
+#                 'fightrecord':test_fighter.fightrecord
+#             }
+#         )
+#         if result:
+#             print('Done.')
 
 
 if __name__ == '__main__':
-    tt_localtest()
+    app.run(debug=True)
